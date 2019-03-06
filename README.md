270810e18777b0d8d701e517c96b289b740eb959
Hyeseong Kim <cometkim.kr@gmail.com>
2019-03-07 03:01:01 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# use-pulled-grid
+
+A React hook provides responsive CSS Grid container
+
+- [x] Calculate CSS properties for grid container to fit the current window width
+- [ ] Get easier to use [animated-css-grid](https://github.com/aholachek/animate-css-grid)
+
+## Usage
+
+```jsx
+import React from 'react'
+import usePulledGrid from 'use-pulled-grid'
+
+const ProductCardList = ({ products }) => {
+  const { styles } = usePulledGrid({
+      columnMinWidth: 100, // Each grid items never get smaller than 100px
+      gridGap: 10,
+  })
+
+  return (
+    <div style={styles.container}>
+      {products.map(product => (
+        {/* Wrappers to each child are required for css-animated-grid and fallback style of grid gap */}
+        <div key={`product-card-wrapper_${product.id}`} style={styles.itemWrapper}>
+            <ProductCard {...product}/>
+        </div>
+      )}
+    </div>
+  )
+}
+```
+
+## License
+
+MIT
---
+++ index.ts
@@ -0,0 +1,64 @@
+import { CSSProperties, useState, useEffect, useMemo, useCallback } from 'react'
+import debounce from 'lodash/debounce'
+
+type PulledGridHookDebounceOption = {
+    delay?: number,
+    leading?: boolean,
+    trailing?: boolean,
+}
+
+type PulledGridHookOption = {
+    columnMinWidth: number,
+    gridGap: number,
+    debounce?: PulledGridHookDebounceOption,
+}
+
+type PulledGridHookOutput = {
+    styles: {
+        container: CSSProperties,
+        itemWrapper: CSSProperties,
+    },
+}
+
+export default function usePulledGrid({
+  columnMinWidth,
+  gridGap,
+  debounce: debounceOption = {},
+}: PulledGridHookOption): PulledGridHookOutput {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  const { delay = 300, leading = false, trailing = true } = debounceOption
+  const handleResize = useCallback(debounce(
+    () => setWindowWidth(window.innerWidth), delay, { leading, trailing }
+  ), [])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  const columns = useMemo(() => (
+    Math.floor((windowWidth - gridGap) / (columnMinWidth + gridGap))
+  ), [columnMinWidth, gridGap, windowWidth])
+
+  const gridContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: `repeat(${columns}, 1fr)`,
+    gap: `${gridGap}px`,
+  }
+
+  const gridItemWrapperStyle = {
+    // A fallback style for browsers which not supports grid-gap property yet.
+    // See the [capability table](https://developer.mozilla.org/en-US/docs/Web/CSS/gap#Support_in_Grid_layout)
+    minWidth: `calc(100% / ${columns} - ${gridGap}px * ${columns} - ${gridGap}px)`,
+  }
+
+  return {
+    styles: {
+      container: gridContainerStyle,
+      itemWrapper: gridItemWrapperStyle,
+    }
+  }
+}
